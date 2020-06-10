c2b7cdd9ee4c13987bdc5b31c56486a3cccb1003
Yue Zhang <github@zhangyue.dev>
2020-06-10 09:04:06 -0400

    Merge sort.

--- src/algorithms/sorting/merge.rs
+++ src/algorithms/sorting/merge.rs
@@ -5,42 +5,43 @@ trait Merge {
 impl<T: PartialOrd + Copy> Merge for [T] {
     fn merge_sort(&mut self) {
         if self.len() > 1 {
-            let pivot = self.len() / 2;
-            let list_1 = &mut self[pivot..];
+            let mid = self.len() / 2;
+            let list_1 = &mut self[mid..];
             list_1.merge_sort();
-            let list_2 = &mut self[..pivot];
+            let list_2 = &mut self[..mid];
             list_2.merge_sort();
 
-            let mut temp: Vec<T> = vec![];
-
-            merge(&self[pivot..], &self[..pivot], &mut temp);
+            let temp = merge(&self[mid..], &self[..mid]);
 
             self.copy_from_slice(&temp);
         }
     }
 }
 
-fn merge<T: PartialOrd + Copy>(list_1: &[T], list_2: &[T], ret: &mut Vec<T>) {
+fn merge<T: PartialOrd + Copy>(list_1: &[T], list_2: &[T]) -> Vec<T> {
     let (mut i, mut j) = (0, 0);
+    let mut result: Vec<T> = vec![];
     while i < list_1.len() && j < list_2.len() {
         if list_1[i] < list_2[j] {
-            ret.push(list_1[i]);
+            result.push(list_1[i]);
             i += 1;
         } else {
-            ret.push(list_2[j]);
+            result.push(list_2[j]);
             j += 1;
         }
     }
 
     while i < list_1.len() {
-        ret.push(list_1[i]);
+        result.push(list_1[i]);
         i += 1;
     }
 
     while j < list_2.len() {
-        ret.push(list_2[j]);
+        result.push(list_2[j]);
         j += 1;
     }
+
+    result
 }
 
 // # Unit Tests
@@ -52,6 +53,7 @@ mod tests {
     fn merge_sort_test() {
         let mut list = vec![3, 2, 1, 4, 6, 5, 7];
         list.merge_sort();
+        println!("{:?}", list);
         assert_ne!(list, vec![3, 2, 1, 4, 6, 4, 7]);
         assert_eq!(list, vec![1, 2, 3, 4, 5, 6, 7]);
     }
